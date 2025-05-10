d518851e076f1131a621d7fc2760c6661b4379fb
billywithaw1lly <139010023+billywithaw1lly@users.noreply.github.com>
2025-05-11 00:06:35 +0530

    warmUp project

---
+++ 01.py
@@ -0,0 +1,13 @@
+print("Welcome to my computer quiz!")
+
+play_on = True
+
+while play_on:
+    playing = input ("wanna play ? Y/N :: ").strip().upper() # handles spaces and converts to upper
+    if playing == 'N':
+        print("okay maybe next time!\n")
+        play_on = False
+    elif playing == 'Y':
+        print("great!!\n")
+    else:
+        print("Invalid Input")
